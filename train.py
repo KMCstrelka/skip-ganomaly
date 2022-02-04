bd27c461b969ac656d6c1d4518372cd36b507450
yuki hada <strelka@tachyon>
2022-02-04 21:08:02 +0900

    設定されたオプションによってtest関数を実行する分岐を追加

--- train.py
+++ train.py
@@ -23,7 +23,10 @@ def main():
     opt = Options().parse()
     data = load_data(opt)
     model = load_model(opt, data)
-    model.train()
+    if self.opt.phase=="train":
+        model.train()
+    elif self.opt.phase=="test":
+        model.test()
 
 if __name__ == '__main__':
     main()
